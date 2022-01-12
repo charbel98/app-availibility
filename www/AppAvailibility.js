3ae685e610bcb0a69bfc22a1713ec36cee5e8fd8
charbel beainy <52603230+charbel98@users.noreply.github.com>
2022-01-12 10:50:26 +0200

    Update AppAvailibility.js
    Add bridge to native toast

--- www/AppAvailibility.js
+++ www/AppAvailibility.js
@@ -3,3 +3,12 @@ var exec = require('cordova/exec');
 exports.isAppRunning = function (arg0, success, error) {
     exec(success, error, 'AppAvailibility', 'isAppRunning', [arg0]);
 };
+exports.nativeToast = function (arg0, success, error) { 
+                 exec(
+                    success, 
+                    error, 
+                   'AppAvailibility', 
+                   'nativeToast', 
+                    [arg0]
+                    ); 
+};
