97a4d57f448e67df9631dcdf86e201536aaa2c80
charbel beainy <52603230+charbel98@users.noreply.github.com>
2022-01-12 11:45:52 +0200

    Update AppAvailibility.js
    remove debugger

--- www/AppAvailibility.js
+++ www/AppAvailibility.js
@@ -3,8 +3,7 @@ var exec = require('cordova/exec');
 exports.isAppRunning = function (arg0, success, error) {
     exec(success, error, 'AppAvailibility', 'isAppRunning', [arg0]);
 };
-exports.nativeToast = function (arg0, success, error) { 
-    debugger;
+exports.nativeToast = function (arg0, success, error) {
                  exec(
                     success, 
                     error, 
