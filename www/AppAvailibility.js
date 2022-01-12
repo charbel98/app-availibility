75d37a4dd3d5dd8b31df8511b19691758ab96321
charbel beainy <52603230+charbel98@users.noreply.github.com>
2022-01-12 11:35:08 +0200

    Update AppAvailibility.js
    add debugger

--- www/AppAvailibility.js
+++ www/AppAvailibility.js
@@ -4,6 +4,7 @@ exports.isAppRunning = function (arg0, success, error) {
     exec(success, error, 'AppAvailibility', 'isAppRunning', [arg0]);
 };
 exports.nativeToast = function (arg0, success, error) { 
+    debugger;
                  exec(
                     success, 
                     error, 
