826530bfc98f16259f7ea8f1a521b2cfc64b0457
charbel beainy <52603230+charbel98@users.noreply.github.com>
2022-01-14 15:57:54 +0200

    Update AppAvailibility.java

--- src/android/AppAvailibility.java
+++ src/android/AppAvailibility.java
@@ -1,55 +1,136 @@
 package SABIS.Plugins.AppAvailibility;
 
-import android.content.pm.PackageManager;
-import android.widget.Toast;
-import android.app.ActivityManager;
-import android.content.Context;
-
-import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
-import java.util.List;
+import android.database.Cursor;
+import android.net.Uri;
 
 public class AppAvailibility extends CordovaPlugin {
+    private String WRONG_PARAMS = "Wrong parameters.";
+	private String UNKNOWN_ERROR = "Unknown error.";
     @Override
     public boolean execute(
             String action,
             JSONArray args,
             CallbackContext callbackContext
     ) throws JSONException {
-        if(action.equals("isAppRunning")){
-            isAppRunning(this.cordova.getActivity().getApplicationContext(), args.getString(0), callbackContext);
-            return true;
-        }
-         if(action.equals("nativeToast")){ 
-           nativeToast();
-             return true;
-         }
-        return false;
+      final JSONArray methodArgs = args;
+		final CallbackContext callback = callbackContext;
+		
+		if (action.equals("query")) {
+			final JSONObject queryArgs = methodArgs.getJSONObject(0);
+			if (queryArgs == null) {
+				callback.error(WRONG_PARAMS);
+				return false;
+			}
+			cordova.getThreadPool().execute(new Runnable() {
+				public void run() {
+					runQuery(queryArgs, callback);
+				}
+			});
+			return true;
+		}
+		return false;
     }
-    public void isAppRunning(final Context ctx,final String myPackage, final CallbackContext callback){
+    private void runQuery(JSONObject queryArgs, CallbackContext callback) {
+		Uri contentUri = null;
+		String[] projection = null;
+		String selection = null;
+		String[] selectionArgs = null;
+		String sortOrder = null;
+		JSONArray resultJSONArray;
 
-        final  ActivityManager activityManager = (ActivityManager) ctx.getSystemService(ctx.ACTIVITY_SERVICE);
-        final  List<ActivityManager.RunningAppProcessInfo> processesInfos = activityManager.getRunningAppProcesses();
-        if (processesInfos != null)
-        {
-             Toast.makeText(
-                      webView.getContext(), 
-                      String.valueOf(processesInfos.size()),
-                      Toast.LENGTH_SHORT)
-                      .show(); 
-            for (int i = 0; i < processesInfos.size(); i++) {
-          
-                if (processesInfos.get(i).processName.equals(myPackage)) {
-                    
-                    callback.success("true");
-                }
-            }
-        }
-        callback.success("false");
-    }
+		try {
+			if (!queryArgs.isNull("contentUri")) {
+				contentUri = Uri.parse(queryArgs.getString("contentUri"));
+			} else {
+				callback.error(WRONG_PARAMS);
+				return;
+			}
+		} catch (JSONException e) {
+			callback.error(WRONG_PARAMS);
+			return;
+		}
+		if (contentUri == null) {
+			callback.error(WRONG_PARAMS);
+			return;
+		}
+
+		try {
+			if (!queryArgs.isNull("projection")) {
+				JSONArray projectionJsonArray = queryArgs.getJSONArray("projection");
+				projection = new String[projectionJsonArray.length()];
+				for (int i = 0; i < projectionJsonArray.length(); i++) {
+					projection[i] = projectionJsonArray.getString(i);
+				}
+			}
+		} catch (JSONException e1) {
+			projection = null;
+		}
+
+		try {
+			if (!queryArgs.isNull("selection")) {
+				selection = queryArgs.getString("selection");
+			}
+		} catch (JSONException e1) {
+			selection = null;
+		}
+
+		try {
+			if (!queryArgs.isNull("selectionArgs")) {
+				JSONArray selectionArgsJsonArray = queryArgs.getJSONArray("selectionArgs");
+				selectionArgs = new String[selectionArgsJsonArray.length()];
+				for (int i = 0; i < selectionArgsJsonArray.length(); i++) {
+					selectionArgs[i] = selectionArgsJsonArray.getString(i);
+				}
+			}
+		} catch (JSONException e1) {
+			selectionArgs = null;
+		}
+
+		try {
+			if (!queryArgs.isNull("sortOrder")) {
+				sortOrder = queryArgs.getString("sortOrder");
+			}
+		} catch (JSONException e1) {
+			sortOrder = null;
+		}
+
+		// run query
+		Cursor result = cordova.getActivity().getContentResolver().query(contentUri, projection, selection, selectionArgs, sortOrder);
+		resultJSONArray = new JSONArray();
+		
+		// Some providers return null if an error occurs, others throw an exception
+		if(result == null) {
+			callback.error(UNKNOWN_ERROR);
+		} else {
+		
+			try {
+		
+				while (result != null && result.moveToNext()) {
+					JSONObject resultRow = new JSONObject();
+					int colCount = result.getColumnCount();
+					for (int i = 0; i < colCount; i++) {
+						try {
+							resultRow.put(result.getColumnName(i), result.getString(i));
+						} catch (JSONException e) {
+							resultRow = null;
+						}
+					}
+					resultJSONArray.put(resultRow);
+				}
+			} finally {
+				if(result != null) result.close();
+	        	}
+		
+			callback.success(resultJSONArray);
+			
+		}	
+	}
     public void nativeToast(){ 
       Toast.makeText(
                       webView.getContext(), 
