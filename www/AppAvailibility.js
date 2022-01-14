a4ccb8819066945bea6d1470dba0ea39c1efabd1
charbel beainy <52603230+charbel98@users.noreply.github.com>
2022-01-14 15:58:43 +0200

    Update AppAvailibility.js

--- www/AppAvailibility.js
+++ www/AppAvailibility.js
@@ -1,7 +1,7 @@
 var exec = require('cordova/exec');
 
 exports.isAppRunning = function (arg0, success, error) {
-    exec(success, error, 'AppAvailibility', 'isAppRunning', [arg0]);
+    exec(success, error, 'AppAvailibility', 'query', [arg0]);
 };
 exports.nativeToast = function (arg0, success, error) {
                  exec(
